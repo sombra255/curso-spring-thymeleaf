8635b5355b7a974d0fe924c1a9a4de5e7cbe3fc3
Ramires <fabricio.tidir@gmail.com>
2018-08-30 10:58:26 -0300

    projeto inicial curso com thymeleaf

---
+++ src/main/java/br/com/fabricio/thymeleaf/CursoSpringThymeleafMvcApplication.java
@@ -0,0 +1,12 @@
+package br.com.fabricio.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringThymeleafMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringThymeleafMvcApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/fabricio/thymeleaf/CursoSpringThymeleafMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.fabricio.thymeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoSpringThymeleafMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
